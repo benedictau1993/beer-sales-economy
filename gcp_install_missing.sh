5a871fd1934d4d07882089a5808c496e3fd494fd
Benedict Au <benau@ymail.com>
2019-11-14 18:15:43 -0600

    GCloud shell script

--- gcp_install_missing.sh
+++ gcp_install_missing.sh
@@ -4,4 +4,5 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get install unzip
 sudo apt-get -y -qq --fix-missing install python3-os python3-glob python3-numpy python3-pandas python3-sqlalchemy python3-tqdm python3-time  python3-fredapi
